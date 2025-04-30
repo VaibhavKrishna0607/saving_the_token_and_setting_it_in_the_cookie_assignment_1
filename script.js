5b189643e13010364e4caa361be481a29e1659d3
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-04-30 14:16:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'your_secret_key';
+
+const encrypt = (payload) => {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+};
+
+const decrypt = (token) => {
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  encrypt,
+  decrypt,
+};
